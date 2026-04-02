c628f9e26712e63fce36f7bca8fa17c18eb9ce38
Mac <mac@Macs-MacBook-Air.local>
2026-04-02 14:02:44 +0300

    Ülesanne 9 tehtud

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 import Header from './components/Header.js'
 import Meals from './components/Meals.js'
 import CartContext from './store/CartContext.js';
+import Modal from './components/UI/Modal.js';
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
@@ -31,17 +32,34 @@ const cartReducer = (state, action) => {
 const App = () => {
 
    const [cartState, dispatchCartAction] = useReducer(cartReducer, { items: [] })
+     const [modalOpen, setModalOpen] = useState(false)
  
    const addItemToCartHandler = (item) => {
      dispatchCartAction({ type: "ADD_ITEM", item })
    };
+const openModalHandler = () => {
+    setModalOpen(true)
+  }
 
+  const closeModalHandler = () => {
+    setModalOpen(false)
+  }
+
+  const checkoutHandler = () => {
+    console.log("Checkout clicked!")
+    setModalOpen(false)
+  }
+
+  const totalItems = cartState.items.reduce((total, item) => total + item.quantity, 0)
  return (
     <>
      <CartContext.Provider value={{ items: cartState.items, addItem: addItemToCartHandler }}>
-    <Header />
+     <Header onOpenCart={openModalHandler} totalItems={totalItems} />
     <main>
     <Meals />
+    <Modal isOpen={modalOpen} onClose={closeModalHandler}>
+           <h2>Test</h2>
+         </Modal>
     </main>
     </CartContext.Provider>
 
