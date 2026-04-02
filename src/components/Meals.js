953e492d9d6e39f800112d63a01652b688e4bcef
Mac <mac@Macs-MacBook-Air.local>
2026-04-02 11:11:21 +0300

    Ülesanne 3 tehtud

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
+
 const Meals = () => {
-    return (
-        <ul id="meals">
-            { 
-                // list of meals
+    const [meals, setMeals] = useState([])
+
+    
+    useEffect(() => {
+        const fetchMeals = async () => {
+            try {
+                const response = await fetch("http://localhost:3001/meals") 
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals.")
+                }
+                const data = await response.json() 
+                console.log(data)
+                setMeals(data)
+            } catch (error) {
+                console.error("Error fetching meals:", error)
             }
-        </ul>
-    )
+   
+        }
+
+        fetchMeals()
+    }, [])
+
+    return null
 }
 
 export default Meals
