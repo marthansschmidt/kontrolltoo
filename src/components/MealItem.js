ffd88c4001c8f976fa9c3d86250bf0810cf73a07
Mac <mac@Macs-MacBook-Air.local>
2026-04-02 12:45:20 +0300

    Ülesanne 5 tehtud

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,11 +1,17 @@
 const MealItem = (props) => {
+
+    const formattedPrice = new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "EUR",
+      }).format(props.price);
+
     return (
         <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.image}`)} alt={props.name} />
                 <div>
                     <h3>{props.name}</h3>
-                    <p className="meal-item-price">{props.price}</p>
+                    <p className="meal-item-price">{formattedPrice}</p>
                     <p className="meal-item-description">{props.description}</p>
                 </div>
                 <div className="meal-item-actions">
